8d0567500aa18e78ae5c9b94291c77e77220f09c
mohammed-hasan007 <mohammed.hasan@freshworks.com>
2023-10-24 11:57:50 +0530

    Added loader & segregated components

--- src/components/events/active-events.tsx
+++ src/components/events/active-events.tsx
@@ -27,6 +27,8 @@ import {
   User,
   Volunteers,
 } from '@prisma/client';
+import ActivityComponent from '../activity';
+import Loader from '../ui/loader';
 
 interface Activities extends Activity {
   events: { volunteers: Volunteers[]; leaders: EventLeader[] } & Event[];
@@ -41,6 +43,7 @@ const ActiveEvents = (props: { activities: Activities[] }) => {
   const [activties, setActivities] = React.useState(activities);
   const [locations, setLocations] = React.useState([...EVENT_LOCATIONS]);
   const [tags, setTags] = React.useState<any>([]);
+  const [date, setDate] = React.useState<DateRange | undefined>();
 
   React.useEffect(() => {
     fetchAllTags();
@@ -80,6 +83,7 @@ const ActiveEvents = (props: { activities: Activities[] }) => {
   };
 
   const clearFilters = async () => {
+    setLoading(true);
     setActivities(await getAllActivitiesFromDB());
     const updatedTags = tags.map((tag) => ({ ...tag, checked: false }));
     setTags(updatedTags);
@@ -89,6 +93,7 @@ const ActiveEvents = (props: { activities: Activities[] }) => {
     }));
     setLocations(updatedLocations);
     setDate(undefined);
+    setLoading(false);
   };
 
   const getFilters = () => {
@@ -129,11 +134,9 @@ const ActiveEvents = (props: { activities: Activities[] }) => {
     }
   };
 
-  const [date, setDate] = React.useState<DateRange | undefined>();
-
   return (
     <>
-      <div className="flex md:justify-between justify-start gap-2 mt-12 flex-wrap md:flex-nowrap">
+      <div className="flex md:flex-row flex-col md:justify-between justify-start gap-4 mt-12 flex-wrap md:flex-nowrap">
         <div className="main-header">
           <h1 className="text-xl">Events - 2023</h1>
         </div>
@@ -160,126 +163,31 @@ const ActiveEvents = (props: { activities: Activities[] }) => {
             disabled={isLoading}
             onUpdate={() => {}}
           />
-          <Button disabled={isLoading} onClick={applyFilter}>
-            Apply filters
-          </Button>
-          <Button
-            variant={'secondary'}
-            disabled={isLoading}
-            onClick={clearFilters}
-          >
-            Clear filters
-          </Button>
+          <div className="flex gap-3">
+            <Button disabled={isLoading} onClick={applyFilter}>
+              Apply filters
+            </Button>
+            <Button
+              variant={'secondary'}
+              disabled={isLoading}
+              onClick={clearFilters}
+            >
+              Clear filters
+            </Button>
+          </div>
         </div>
       </div>
+      {isLoading && <Loader />}
       {activties.length ? (
         activties?.map((activity) => {
           return (
             <div
-              className="container h-auto px-0 mx-auto my-10 border border-b-2 shadow w-100 bg-grey rounded-xl bg-card text-card-foreground "
+              className={`container h-auto px-0 mx-auto my-10 border border-b-2 shadow w-100 bg-grey rounded-xl bg-card text-card-foreground ${
+                isLoading ? 'opacity-50' : ''
+              }`}
               key={activity.id}
             >
-              <div className="flex flex-wrap gap-6 p-4 mx-auto event-container md:flex-nowrap md:h-600">
-                <Link
-                  className="flex lg:w-1/3 lg:h-100 max-h-72"
-                  href={`/activities/${activity.id}`}
-                >
-                  <img
-                    alt="ecommerce"
-                    className="object-cover object-center w-full rounded"
-                    src={activity.cover || ''}
-                  />
-                </Link>
-
-                <div className="w-full mt-6 lg:w-1/3 lg:mt-0">
-                  <h1 className="mb-1 text-xl font-semibold text-gray-700 title-font">
-                    {activity.name}
-                  </h1>
-                  <div className="flex gap-2 mb-2 text-gray-700 location items-center">
-                    <MapPin size="18" />
-                    <p className="text-sm">{activity?.city}</p>
-                  </div>
-                  <h2 className="mb-1 text-sm tracking-widest text-gray-400 title-font">
-                    {activity?.tags.length > 0
-                      ? activity?.tags
-                          .map((tag) => `#${tag?.tag.name}`)
-                          .join(', ')
-                      : 'No tags available'}
-                  </h2>
-
-                  <p className="leading-relaxed">
-                    {getActivityDescription(activity)}
-                  </p>
-                  <div className="flex items-center pb-5 mt-2 mb-0 text-sm">
-                    <div className="flex flex-col gap-y-2">
-                      <p>
-                        <strong className="mr-3">Duration: </strong>
-                        {activity?.duration} minutes
-                        {/* <time
-                          dateTime={convertToReadableDate(activity.startTime)}
-                          suppressHydrationWarning
-                        >
-                          {convertToReadableDate(activity.startTime)} to{' '}
-                          {convertToReadableDate(activity.endTime)}
-                        </time> */}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <Link href={`/activities/${activity.id}`}>
-                      <Button size="sm" variant="outline">
-                        Learn more
-                      </Button>
-                    </Link>
-                  </div>
-                </div>
-
-                {/* <div className="inline-block h-[250px] min-h-[1em] w-1 self-stretch bg-black opacity-100"></div> */}
-
-                <div className="w-full pl-5 participants lg:w-1/3 ">
-                  <h2 className="pb-2 mb-2 text-xl font-medium text-gray-700 ">
-                    Events
-                  </h2>
-                  {activity.events.map((event: any) => (
-                    <React.Fragment key={event.id}>
-                      <Separator className="mb-4 mt-46" />
-                      <Link href={`/activities/${activity.id}`}>
-                        <div className="join-event">
-                          <div className="flex justify-between my-2 text-sm text-gray-700 event-timings align-center">
-                            {' '}
-                            {/* <time
-                              dateTime={convertToReadableDate(
-                                activity.startTime,
-                              )}
-                              suppressHydrationWarning
-                            >
-                              {shortenDate(event.startTime)} to{' '}
-                              {shortenDate(event.endTime)}
-                            </time> */}
-                            <div className="flex items-center text-sm text-gray-700 event-duration gap-x-1">
-                              <CalendarRangeIcon size="18" />
-                              {event.is_dates_announced
-                                ? ((new Date(
-                                    event.endTime as any,
-                                  ).getTime() as any) -
-                                    (new Date(
-                                      event.startTime as any,
-                                    ).getTime() as any)) /
-                                    (1000 * 60 * 60) +
-                                  ' hrs'
-                                : event.date_announcement_text}
-                            </div>
-                          </div>
-                          <EventJoinButton
-                            event={event}
-                            alreadyJoinedActivity={true}
-                          />
-                        </div>
-                      </Link>
-                    </React.Fragment>
-                  ))}
-                </div>
-              </div>
+              <ActivityComponent activity={activity} />
             </div>
           );
         })

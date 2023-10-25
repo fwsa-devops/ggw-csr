13bc7435ff700cf4f3d9c1ed6e2e13eba34eb775
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-10-25 13:02:35 +0530

    minor: create users on sign in

--- src/app/(api-routes)/api/auth/[...nextauth]/route.ts
+++ src/app/(api-routes)/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 
 import GoogleProvider from 'next-auth/providers/google';
 import { NextAuthOptions } from 'next-auth';
+import prisma from '@/lib/prisma';
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -14,6 +15,44 @@ const authOptions: NextAuthOptions = {
   theme: {
     colorScheme: 'light' as const,
   },
+  events: {
+    createUser: async ({ user }) => {
+
+      const { email, image, name } = user;
+      console.log("createUser", user)
+      await prisma.user.create({
+        data: {
+          email: email as string,
+          name: name as string,
+          image: image ?? null
+        }
+      })
+
+    },
+    signIn: async ({ user }) => {
+      console.log("signIn", user)
+
+      const { email, image, name } = user;
+
+      console.log("newUser");
+      await prisma.user.upsert({
+        where: {
+          email: email as string
+        },
+        update: {
+          name: name as string,
+          image: image ?? null
+        },
+        create: {
+          email: email as string,
+          name: name as string,
+          image: image ?? null
+        },
+      })
+
+    }
+
+  }
 };
 
 const handler = NextAuth(authOptions);

e1f5684930a08edbf01e52d3377d0a7632deb4fa
Surya Umapathy <suryaumapathy2812@gmail.com>
2024-02-24 10:41:29 +0530

    fix: added website title & desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,15 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import SessionProvider from '@/components/providers/session';
 import Header from '@/components/shared/header';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Event Management System',
+  description:
+    'Event Management System for managing events and attendees with ease.',
+};
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (

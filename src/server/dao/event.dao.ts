2ba9835b79f220879eef16e8d8720ec844f1a5d2
Surya Umapathy <32648210+suryaumapathy2812@users.noreply.github.com>
2024-06-14 18:52:21 +0530

    Update event.dao.ts

--- src/server/dao/event.dao.ts
+++ src/server/dao/event.dao.ts
@@ -144,7 +144,7 @@ export async function create(email: string, data: INewEvent) {
         },
         Address: {
           create: {
-            name: data.address.name,
+            name: data.address.name ?? null,
             street: data.address.street,
             city: data.address.city,
             state: data.address.state,
